309b6fd780b0e6f9e48c42a10a5f906422252e02
tredeaux <tredeaux.pitout@gmail.com>
2025-08-26 15:49:44 +0200

    render yaml

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
@@ -10,6 +11,14 @@ async function bootstrap() {
         credentials: false,
     });
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true }));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Test API')
+        .setDescription('API documentation')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('docs', app, document);
     const port = Number(process.env.PORT ?? 3000);
     await app.listen(port);
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,14 @@ async function bootstrap() {
     credentials: false,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Test API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   const port = Number(process.env.PORT ?? 3000);
   await app.listen(port);
 }
